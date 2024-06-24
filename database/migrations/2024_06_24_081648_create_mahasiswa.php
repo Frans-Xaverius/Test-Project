01964c39401a2afa2d27d62ee75a436f6a027422
FX <75465898+Frans-Xaverius@users.noreply.github.com>
2024-06-24 16:29:35 +0800

    Migrate

---
+++ database/migrations/2024_06_24_081648_create_mahasiswa.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim');
+            $table->string('nama');
+            $table->integer('fakultas_id');
+            $table->integer('prodi_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
