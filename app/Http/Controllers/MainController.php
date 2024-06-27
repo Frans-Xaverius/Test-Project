188c5f34908d3b89172d7800fcd3c7e5b613b0fb
RaidyMR <raidymustafid39@gmail.com>
2024-06-27 08:02:31 +0700

    simplify controller and routes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,123 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prodi;
+use App\Models\Fakultas;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class MainController extends Controller {
 
-    public function index () {
+    public function index()
+    {
+        $mahasiswas = $mahasiswas = Mahasiswa::all(); // Fetch all Mahasiswa records
+        return view('main.index', compact('mahasiswas')); // Return the view with the Mahasiswa records
+    }
 
-        return view('main.index');
+    public function destroy($id)
+    {
+        $mahasiswa = Mahasiswa::find($id); // Find the Mahasiswa record by ID
+        $mahasiswa->delete(); // Delete the Mahasiswa record
+
+        return redirect()->route('main.index'); // Redirect to the Mahasiswa index page
+    }
+
+    // Method to show create form
+    public function create()
+    {
+        $prodis = Prodi::all();
+        $fakultases = Fakultas::all();
+        return view('main.create', compact('prodis', 'fakultases'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'prodi_id' => 'required|exists:prodis,id',
+            'fakultas_id' => 'required|exists:fakultases,id',
+        ]);
+
+        // get prodi kode and fakultas kode using their id
+        $prodi = Prodi::find($request->prodi_id);
+        $fakultas = Fakultas::find($request->fakultas_id);
+
+        // create nim
+        $nim = '10' . $fakultas->kode . $prodi->kode . '24';
+
+        // find the last nim
+        $lastNim = Mahasiswa::where('nim', 'like', $nim . '%')->orderBy('nim', 'desc')->first();
+
+        // if there is a last nim, increment the last 3 digits
+        if ($lastNim) {
+            $lastNim = (int) substr($lastNim->nim, -3);
+            $lastNim++;
+            $nim .= str_pad($lastNim, 3, '0', STR_PAD_LEFT);
+        } else {
+            $nim .= '001';
+        }
+
+        // create a new Mahasiswa record
+        Mahasiswa::create([
+            'nama' => $request->nama,
+            'nim' => $nim,
+            'prodi_id' => $request->prodi_id,
+            'fakultas_id' => $request->fakultas_id,
+        ]);
+
+        return redirect()->route('main.index')
+            ->with('success', 'Mahasiswa added successfully.');
+    }
+
+    // Method to show edit form
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        $prodis = Prodi::all();
+        $fakultases = Fakultas::all();
+        return view('main.edit', compact('mahasiswa', 'prodis', 'fakultases'));
+    }
+
+    // Method to update Mahasiswa record
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'prodi_id' => 'required|exists:prodis,id',
+            'fakultas_id' => 'required|exists:fakultases,id',
+        ]);
+
+
+
+        // get prodi kode and fakultas kode using their id
+        $prodi = Prodi::find($request->prodi_id);
+        $fakultas = Fakultas::find($request->fakultas_id);
+
+        // create nim
+        $nim = '10' . $fakultas->kode . $prodi->kode . '24';
+
+        // find the last nim
+        $lastNim = Mahasiswa::where('nim', 'like', $nim . '%')->orderBy('nim', 'desc')->first();
+
+        // if there is a last nim, increment the last 3 digits
+        if ($lastNim) {
+            $lastNim = (int) substr($lastNim->nim, -3);
+            $lastNim++;
+            $nim .= str_pad($lastNim, 3, '0', STR_PAD_LEFT);
+        } else {
+            $nim .= '001';
+        }
+
+        $mahasiswa->update(
+            [
+                'nama' => $request->nama,
+                'nim' => $nim,
+                'prodi_id' => $request->prodi_id,
+                'fakultas_id' => $request->fakultas_id,
+            ]
+        );
+
+        return redirect()->route('main.index')
+            ->with('success', 'Mahasiswa updated successfully.');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormController;
 use App\Http\Controllers\MainController;
-use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +15,12 @@ use App\Http\Controllers\MahasiswaController;
 */
 
 Route::prefix('main')->group(function(){
-    Route::get('/', [MainController::class, 'index'])->name('main');
+    Route::get('/', [MainController::class, 'index'])->name('main.index');
+    Route::get('/create', [MainController::class, 'create'])->name('main.create');
+    Route::get('/{mahasiswa}/edit', [MainController::class, 'edit'])->name('main.edit');
+    Route::post('/store', [MainController::class, 'store'])->name('main.store');
+    Route::put('/{mahasiswa}', [MainController::class, 'update'])->name('main.update');
+    Route::delete('/{id}', [MainController::class, 'destroy'])->name('main.destroy');
 });
 
-Route::get('/daftar', function () {
-    return view('daftar');
-});
-
-Route::post('/submit-form', [FormController::class, 'submit'])->name('form.submit');
-
-Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
-
-Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
+// Route::get('', [MainController::class, 'index'])->name('main.index');
